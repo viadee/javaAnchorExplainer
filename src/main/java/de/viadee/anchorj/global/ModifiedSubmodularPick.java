d6383885376f35a1b0e37fba14c827110df3d040
Tobias Goerke <tobias.goerke@gmail.com>
2018-11-23 15:27:49 +0100

    Fixed submodular pick

--- src/main/java/de/viadee/anchorj/global/ModifiedSubmodularPick.java
+++ src/main/java/de/viadee/anchorj/global/ModifiedSubmodularPick.java
@@ -1,19 +1,18 @@
 package de.viadee.anchorj.global;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import de.viadee.anchorj.AnchorConstruction;
 import de.viadee.anchorj.AnchorConstructionBuilder;
 import de.viadee.anchorj.AnchorResult;
 import de.viadee.anchorj.DataInstance;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Class implementing the Submodular Pick (SP) algorithm proposed by Ribeiro (2016).
  * <p>
- * In the showcase example this class gets extended and enabled to be used in conjunction with Apache Spark.
- * Due to this projects dependency-less design, it is not included in the core project.
+ * This class extends the SP algorithm to account not only for feature ids but also for their values
  */
 public class ModifiedSubmodularPick<T extends DataInstance<?>> extends SubmodularPick<T> {
 
@@ -42,11 +41,10 @@ public class ModifiedSubmodularPick<T extends DataInstance<?>> extends Submodula
     }
 
     @Override
-    protected double[][] createImportanceMatrix(AnchorResult<T>[] anchorResults) {
-        // Build matrix
-        // 2. Assign a unique column uniqueIndex to each feature
-        final Map<Integer, Map<Object, Integer>> featureValueIndexMap = new HashMap<>();
+    protected CreateFeatureToColumnMapResult createFeatureToColumnMap(AnchorResult<T>[] anchorResults) {
         final AtomicInteger uniqueIndex = new AtomicInteger();
+        final Map<Integer, Map<Object, Integer>> featureValueIndexMap = new HashMap<>();
+
         for (final AnchorResult<T> anchorResult : anchorResults)
             for (final Integer feature : anchorResult.getOrderedFeatures()) {
                 Object featureValue = anchorResult.getInstance().getValue(feature);
@@ -54,16 +52,13 @@ public class ModifiedSubmodularPick<T extends DataInstance<?>> extends Submodula
                         .computeIfAbsent(feature, (v) -> new HashMap<>())
                         .computeIfAbsent(featureValue, (v) -> uniqueIndex.getAndIncrement());
             }
+        return new CreateFeatureToColumnMapResult(featureValueIndexMap, uniqueIndex.get());
+    }
 
-        // 3. Calculate cell importance matrix, which later gets transformed to importance matrix
-        final double[][] importanceMatrix = new double[anchorResults.length][uniqueIndex.get()];
-        for (int i = 0; i < importanceMatrix.length; i++) {
-            for (final int feature : anchorResults[i].getOrderedFeatures()) {
-                Integer colIndex = featureValueIndexMap.get(feature)
-                        .get(anchorResults[i].getInstance().getValue(feature));
-                importanceMatrix[i][colIndex] = computeFeatureImportance(anchorResults[i], feature);
-            }
-        }
-        return importanceMatrix;
+    @Override
+    @SuppressWarnings("unchecked")
+    protected int getCandidateFeatureIndex(Map<Integer, ?> featureToColumnMap, AnchorResult anchorResult, int feature) {
+        return ((Map<Integer, Map<Object, Integer>>) featureToColumnMap)
+                .get(feature).get(anchorResult.getInstance().getValue(feature));
     }
 }
--- src/main/java/de/viadee/anchorj/global/SubmodularPick.java
+++ src/main/java/de/viadee/anchorj/global/SubmodularPick.java
@@ -17,7 +17,7 @@ import java.util.*;
  * <li>Obtain explanations for all instances</li>
  * <li>Create a 2D matrix. Store a row for each explanation and map it to the explanation's feature importance</li>
  * <li>Flatten this matrix to obtain feature importance of a whole feature</li>
- * <li>Now select explanations s.t. they optimize these values</li>
+ * <li>Now select explanations s.t. they optimize these values using a greedy algorithm</li>
  * </ol>
  */
 public class SubmodularPick<T extends DataInstance<?>> {
@@ -57,7 +57,7 @@ public class SubmodularPick<T extends DataInstance<?>> {
      * @param importanceMatrix the importance matrix
      * @return the column importance I
      */
-    protected double[] createColumnImportance(double[][] importanceMatrix) {
+    private double[] createColumnImportance(double[][] importanceMatrix) {
         final double[] columnImportances = new double[importanceMatrix[0].length];
         // Loop columns
         for (int columnIndex = 0; columnIndex < importanceMatrix[0].length; columnIndex++) {
@@ -86,7 +86,7 @@ public class SubmodularPick<T extends DataInstance<?>> {
      * @param feature      the feature being examined
      * @return an importance value
      */
-    protected double computeFeatureImportance(AnchorResult<T> anchorResult, int feature) {
+    private double computeFeatureImportance(AnchorResult<T> anchorResult, int feature) {
         // Searches for the parent in which the features has been added and extracts its added feature value
         AnchorCandidate current = anchorResult;
         do {
@@ -118,7 +118,19 @@ public class SubmodularPick<T extends DataInstance<?>> {
         final AnchorResult<T>[] anchorResults = batchExplainer.obtainAnchors(constructionBuilder, instances);
         LOGGER.info("Took {} ms for gathering all explanations", (System.currentTimeMillis() - startTime));
 
-        final double[][] importanceMatrix = createImportanceMatrix(anchorResults);
+        // Build matrix
+        // 2. Assign a unique column index to each feature
+        final CreateFeatureToColumnMapResult createFeatureToColumnMapResult = createFeatureToColumnMap(anchorResults);
+        final Map<Integer, ?> featureToColumnMap = createFeatureToColumnMapResult.map;
+
+        // 3. Calculate cell importance matrix, which later gets transformed to importance matrix
+        final double[][] importanceMatrix = new double[anchorResults.length][createFeatureToColumnMapResult.columnCount];
+        for (int i = 0; i < anchorResults.length; i++) {
+            for (final int feature : anchorResults[i].getOrderedFeatures()) {
+                importanceMatrix[i][getCandidateFeatureIndex(featureToColumnMap, anchorResults[i], feature)] =
+                        computeFeatureImportance(anchorResults[i], feature);
+            }
+        }
 
         // 4. Flatten matrix to see how important each column is. Results in importance matrix I
         final double[] columnImportance = createColumnImportance(importanceMatrix);
@@ -126,24 +138,43 @@ public class SubmodularPick<T extends DataInstance<?>> {
         return greedyPick(nrOfExplanationsDesired, anchorResults, importanceMatrix, columnImportance);
     }
 
-    protected double[][] createImportanceMatrix(final AnchorResult<T>[] anchorResults) {
-        // Build matrix
-        // 2. Assign a unique column index to each feature
+    /**
+     * Creates a mapping for an explanation's feature value to an id
+     *
+     * @param anchorResults the explanations
+     * @return a mapping of unique ids to objects (e.g. features ids or feature values)
+     */
+    protected CreateFeatureToColumnMapResult createFeatureToColumnMap(final AnchorResult<T>[] anchorResults) {
         final Map<Integer, Integer> featureToColumnMap = new HashMap<>();
         int index = 0;
         for (final AnchorResult anchorResult : anchorResults)
             for (final Integer feature : anchorResult.getOrderedFeatures())
                 if (!featureToColumnMap.containsKey(feature))
                     featureToColumnMap.put(feature, index++);
+        return new CreateFeatureToColumnMapResult(featureToColumnMap, featureToColumnMap.size());
+    }
 
-        // 3. Calculate cell importance matrix, which later gets transformed to importance matrix
-        final double[][] importanceMatrix = new double[anchorResults.length][featureToColumnMap.size()];
-        for (int i = 0; i < anchorResults.length; i++) {
-            for (final int feature : anchorResults[i].getOrderedFeatures()) {
-                importanceMatrix[i][featureToColumnMap.get(feature)] = computeFeatureImportance(anchorResults[i], feature);
-            }
+    class CreateFeatureToColumnMapResult {
+        final Map<Integer, ?> map;
+        final int columnCount;
+
+        CreateFeatureToColumnMapResult(Map<Integer, ?> map, int columnCount) {
+            this.map = map;
+            this.columnCount = columnCount;
         }
-        return importanceMatrix;
+    }
+
+    /**
+     * Resolves an explanation's feature to a unique id
+     *
+     * @param featureToColumnMap the featureToColumnMap as computed by {@link #createFeatureToColumnMap(AnchorResult[])}
+     * @param anchorResult       the result whose feature to resolve to an id
+     * @param feature            the feature to resolve
+     * @return the explanation's feature id
+     */
+    @SuppressWarnings("unchecked")
+    protected int getCandidateFeatureIndex(final Map<Integer, ?> featureToColumnMap, AnchorResult anchorResult, int feature) {
+        return ((Map<Integer, Integer>) featureToColumnMap).get(feature);
     }
 
     private List<AnchorResult<T>> greedyPick(final int nrOfExplanationsDesired, final AnchorResult<T>[] anchorResults,
@@ -151,6 +182,7 @@ public class SubmodularPick<T extends DataInstance<?>> {
         // 5. Greedy SP algorithm
         final Set<Integer> selectedIndices = new LinkedHashSet<>();
         final Set<AnchorResult> remainingResults = new HashSet<>(Arrays.asList(anchorResults));
+        double previousBestCoverage = 0;
         for (int i = 0; i < Math.min(nrOfExplanationsDesired, anchorResults.length); i++) {
             double bestCoverage = 0;
             Integer bestIndex = null;
@@ -166,6 +198,10 @@ public class SubmodularPick<T extends DataInstance<?>> {
             if (bestIndex == null)
                 break;
 
+            LOGGER.info("Adding candidate {} adding coverage of {}, totalling to {}",
+                    anchorResults[bestIndex].getCanonicalFeatures(),
+                    (bestCoverage - previousBestCoverage), bestCoverage);
+            previousBestCoverage = bestCoverage;
             selectedIndices.add(bestIndex);
             remainingResults.remove(anchorResults[bestIndex]);
         }
