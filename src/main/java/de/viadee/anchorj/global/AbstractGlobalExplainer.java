91c9043b603da5577ec072f98eb2d8ef3bd48404
Tobias Goerke <tobias.goerke@gmail.com>
2018-11-24 09:49:49 +0100

    Created abstract global explainer providing default functionality for obtaining multiple explanations

---
+++ src/main/java/de/viadee/anchorj/global/AbstractGlobalExplainer.java
@@ -0,0 +1,80 @@
+package de.viadee.anchorj.global;
+
+import de.viadee.anchorj.AnchorConstruction;
+import de.viadee.anchorj.AnchorConstructionBuilder;
+import de.viadee.anchorj.AnchorResult;
+import de.viadee.anchorj.DataInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Provides default functionality for obtaining multiple explanations.
+ * <p>
+ * May be used for all model-agnostic aggregation based explainers
+ *
+ * @param <T> Type of the instance
+ */
+abstract class AbstractGlobalExplainer<T extends DataInstance<?>> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SubmodularPick.class);
+
+    protected final BatchExplainer<T> batchExplainer;
+    protected final AnchorConstructionBuilder<T> constructionBuilder;
+
+    /**
+     * Creates the instance.
+     *
+     * @param constructionBuilder the builder used to create instances of the {@link AnchorConstruction}
+     *                            when running the algorithm.
+     * @param maxThreads          the number of threads to obtainAnchors in parallel.
+     *                            Note: if threading is enabled in the anchorConstructionBuilder, the actual
+     *                            thread count multiplies if executed locally.
+     */
+    public AbstractGlobalExplainer(AnchorConstructionBuilder<T> constructionBuilder, int maxThreads) {
+        this(new ThreadedBatchExplainer<>(maxThreads), constructionBuilder);
+    }
+
+    /**
+     * Creates the instance.
+     *
+     * @param batchExplainer      the {@link BatchExplainer} to be used to obtain multiple explanations
+     * @param constructionBuilder the builder used to create instances of the {@link AnchorConstruction}
+     *                            when running the algorithm.
+     */
+    public AbstractGlobalExplainer(BatchExplainer<T> batchExplainer, AnchorConstructionBuilder<T> constructionBuilder) {
+        this.batchExplainer = batchExplainer;
+        this.constructionBuilder = constructionBuilder;
+    }
+
+    /**
+     * Executes the global explainer.
+     * <p>
+     * Produces multiple results that are most important for the global explanation of a model.
+     *
+     * @param instances               the instances to obtain explanations for.
+     * @param nrOfExplanationsDesired the max result number to generate
+     * @return a {@link List} of {@link AnchorResult}s
+     */
+    public List<AnchorResult<T>> run(final List<T> instances, final int nrOfExplanationsDesired) {
+        if (instances == null || instances.isEmpty())
+            return Collections.emptyList();
+
+        final double startTime = System.currentTimeMillis();
+        // 1. Obtain the anchors - explanation matrix W
+        final AnchorResult<T>[] anchorResults = batchExplainer.obtainAnchors(constructionBuilder, instances);
+        LOGGER.info("Took {} ms for gathering all explanations", (System.currentTimeMillis() - startTime));
+
+        return pickExplanations(anchorResults, nrOfExplanationsDesired);
+    }
+
+    /**
+     * Executes the actual algorithm using the aggregated explanations
+     *
+     * @param explanations            the explanations
+     * @param nrOfExplanationsDesired desired number of explanations to pick
+     * @return the AnchorResults
+     */
+    abstract List<AnchorResult<T>> pickExplanations(final AnchorResult<T>[] explanations, final int nrOfExplanationsDesired);
+}
--- src/main/java/de/viadee/anchorj/global/SubmodularPick.java
+++ src/main/java/de/viadee/anchorj/global/SubmodularPick.java
@@ -20,35 +20,31 @@ import java.util.*;
  * <li>Now select explanations s.t. they optimize these values using a greedy algorithm</li>
  * </ol>
  */
-public class SubmodularPick<T extends DataInstance<?>> {
+public class SubmodularPick<T extends DataInstance<?>> extends AbstractGlobalExplainer<T> {
     private static final Logger LOGGER = LoggerFactory.getLogger(SubmodularPick.class);
 
-    private final BatchExplainer<T> batchExplainer;
-    private final AnchorConstructionBuilder<T> constructionBuilder;
-
     /**
-     * Creates an instance of the {@link SubmodularPick}.
+     * Creates the instance.
      *
      * @param constructionBuilder the builder used to create instances of the {@link AnchorConstruction}
      *                            when running the algorithm.
      * @param maxThreads          the number of threads to obtainAnchors in parallel.
      *                            Note: if threading is enabled in the anchorConstructionBuilder, the actual
-     *                            thread count multiplies.
+     *                            thread count multiplies if executed locally.
      */
     public SubmodularPick(AnchorConstructionBuilder<T> constructionBuilder, int maxThreads) {
-        this(new ThreadedBatchExplainer<>(maxThreads), constructionBuilder);
+        super(constructionBuilder, maxThreads);
     }
 
     /**
-     * Creates an instance of the {@link SubmodularPick}.
+     * Creates the instance.
      *
      * @param batchExplainer      the {@link BatchExplainer} to be used to obtain multiple explanations
      * @param constructionBuilder the builder used to create instances of the {@link AnchorConstruction}
      *                            when running the algorithm.
      */
     public SubmodularPick(BatchExplainer<T> batchExplainer, AnchorConstructionBuilder<T> constructionBuilder) {
-        this.batchExplainer = batchExplainer;
-        this.constructionBuilder = constructionBuilder;
+        super(batchExplainer, constructionBuilder);
     }
 
     /**
@@ -100,42 +96,27 @@ public class SubmodularPick<T extends DataInstance<?>> {
         throw new RuntimeException("Should not happen - Inconsistent candidate inheritance!");
     }
 
-    /**
-     * Executes the {@link SubmodularPick} algorithm.
-     * <p>
-     * Produces multiple results that are most important for the global explanation of a model.
-     *
-     * @param instances               the instances to obtain explanations for.
-     * @param nrOfExplanationsDesired the max result number to generate
-     * @return a {@link List} of {@link AnchorResult}s
-     */
-    public List<AnchorResult<T>> run(final List<T> instances, final int nrOfExplanationsDesired) {
-        if (instances == null || instances.isEmpty())
-            return Collections.emptyList();
 
-        final double startTime = System.currentTimeMillis();
-        // 1. Obtain the anchors - explanation matrix W
-        final AnchorResult<T>[] anchorResults = batchExplainer.obtainAnchors(constructionBuilder, instances);
-        LOGGER.info("Took {} ms for gathering all explanations", (System.currentTimeMillis() - startTime));
-
+    @Override
+    List<AnchorResult<T>> pickExplanations(AnchorResult<T>[] explanations, int nrOfExplanationsDesired) {
         // Build matrix
         // 2. Assign a unique column index to each feature
-        final CreateFeatureToColumnMapResult createFeatureToColumnMapResult = createFeatureToColumnMap(anchorResults);
+        final CreateFeatureToColumnMapResult createFeatureToColumnMapResult = createFeatureToColumnMap(explanations);
         final Map<Integer, ?> featureToColumnMap = createFeatureToColumnMapResult.map;
 
         // 3. Calculate cell importance matrix, which later gets transformed to importance matrix
-        final double[][] importanceMatrix = new double[anchorResults.length][createFeatureToColumnMapResult.columnCount];
-        for (int i = 0; i < anchorResults.length; i++) {
-            for (final int feature : anchorResults[i].getOrderedFeatures()) {
-                importanceMatrix[i][getCandidateFeatureIndex(featureToColumnMap, anchorResults[i], feature)] =
-                        computeFeatureImportance(anchorResults[i], feature);
+        final double[][] importanceMatrix = new double[explanations.length][createFeatureToColumnMapResult.columnCount];
+        for (int i = 0; i < explanations.length; i++) {
+            for (final int feature : explanations[i].getOrderedFeatures()) {
+                importanceMatrix[i][getCandidateFeatureIndex(featureToColumnMap, explanations[i], feature)] =
+                        computeFeatureImportance(explanations[i], feature);
             }
         }
 
         // 4. Flatten matrix to see how important each column is. Results in importance matrix I
         final double[] columnImportance = createColumnImportance(importanceMatrix);
 
-        return greedyPick(nrOfExplanationsDesired, anchorResults, importanceMatrix, columnImportance);
+        return greedyPick(nrOfExplanationsDesired, explanations, importanceMatrix, columnImportance);
     }
 
     /**
@@ -152,16 +133,6 @@ public class SubmodularPick<T extends DataInstance<?>> {
                 if (!featureToColumnMap.containsKey(feature))
                     featureToColumnMap.put(feature, index++);
         return new CreateFeatureToColumnMapResult(featureToColumnMap, featureToColumnMap.size());
-    }
-
-    class CreateFeatureToColumnMapResult {
-        final Map<Integer, ?> map;
-        final int columnCount;
-
-        CreateFeatureToColumnMapResult(Map<Integer, ?> map, int columnCount) {
-            this.map = map;
-            this.columnCount = columnCount;
-        }
     }
 
     /**
@@ -210,5 +181,15 @@ public class SubmodularPick<T extends DataInstance<?>> {
         for (final Integer idx : selectedIndices)
             result.add(anchorResults[idx]);
         return result;
+    }
+
+    class CreateFeatureToColumnMapResult {
+        final Map<Integer, ?> map;
+        final int columnCount;
+
+        CreateFeatureToColumnMapResult(Map<Integer, ?> map, int columnCount) {
+            this.map = map;
+            this.columnCount = columnCount;
+        }
     }
 }
